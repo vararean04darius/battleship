9874fd759d30975383e48341aa0fd18470bab111
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2024-11-11 15:50:35 +0200

    player table created

--- battleboard.js
+++ battleboard.js
@@ -1,2 +1,20 @@
 
 const container = document.getElementById("container");
+
+function createBoard() {
+    const table = document.createElement("div")
+    table.id = "table-holder"
+    container.appendChild(table)
+
+    const player1Table = document.createElement("div")
+    player1Table.classList.add("table")
+    player1Table.classList.add("player1")
+    const player2Table = document.createElement("div")
+    player2Table.classList.add("table")
+    player2Table.classList.add("player2")
+
+    table.appendChild(player1Table)
+    table.appendChild(player2Table)
+}
+
+createBoard();
