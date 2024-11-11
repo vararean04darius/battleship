4105894feb38aae94641492dcc62305f6bf0bd89
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2024-11-11 14:46:18 +0200

    gameboard class and tests finished

--- gameboard.js
+++ gameboard.js
@@ -2,46 +2,61 @@ const Ship = require('./ship');
 
 class Gameboard {
     constructor() {
-        this.ships = 0;
+        this.ships = [];
         this.hits = 0;
         this.missed = 0;
-        //10x10 board
         this.boardMatrix = new Array(10);
         for(let i = 0; i < this.boardMatrix.length; i++) {
             this.boardMatrix[i] = new Array(10);
         }
+        this.hitMatrix = new Array(10);
+        for(let i = 0; i < this.hitMatrix.length; i++) {
+            this.hitMatrix[i] = new Array(10);
+        }
     }
 
-    addShip(x, y, length, direction) {
-        // x, y >= 0 <=9
-        // length >= 1 <= 4
-        // direction 'row' || 'col'       
+    addShip(x, y, length, direction) {   
         let newShip = new Ship(length);
-        
+        let newPair = [x, y];
+        this.ships.push(newPair);
         if(direction == 'row') {
-            while(length >= 1) {
-                this.boardMatrix[x][y] = newShip;
-                x++;
-                length--;
-            }
-        } else {
             while(length >= 1) {
                 this.boardMatrix[x][y] = newShip;
                 y++;
                 length--;
             }
+        } else {
+            while(length >= 1) {
+                this.boardMatrix[x][y] = newShip;
+                x++;
+                length--;
+            }
         }
-        this.ships++
     }
 
     receiveAttack(x, y) {
         if(this.boardMatrix[x][y] != undefined) {
             this.boardMatrix[x][y].hit();
+            this.hitMatrix[x][y] = 'h'
             this.hits++;
         } else {
-            this.boardMatrix[x][y] = 'x';
+            this.hitMatrix[x][y] = 'm'
             this.missed++;
         }
+    }
+    allSunk() {
+        console.log(this.ships)
+        for(let i = 0; i < this.ships.length; i++) {
+            if(this.boardMatrix[this.ships[i][0]][this.ships[i][1]].isSunk() == false) {
+                return false;
+            }
+        }
+        // this.ships.forEach(el => {
+        //     if(this.boardMatrix[el[0]][el[1]].isSunk() == false) {
+        //         return false;
+        //     }
+        // });
+        return true;
     }
 }
 
--- gameboard.test.js
+++ gameboard.test.js
@@ -24,28 +24,82 @@ test("Ship with length 1 is added correctly", () => {
 })
 
 test("Ship with length 2 is added correctly", () => {
-    myGameboard.addShip(2, 1, 2, "row")
-    expect(typeof myGameboard.boardMatrix[2][1]).toBe('object')
-    expect(typeof myGameboard.boardMatrix[3][1]).toBe('object')
+    myGameboard.addShip(1, 2, 2, "row")
+    expect(typeof myGameboard.boardMatrix[1][2]).toBe('object')
+    expect(typeof myGameboard.boardMatrix[1][3]).toBe('object')
     expect(myGameboard.boardMatrix[1][1]).toBe(undefined)
-    expect(myGameboard.boardMatrix[4][1]).toBe(undefined)
-    expect(myGameboard.boardMatrix[3][1].hitTimes).toBe(0)
+    expect(myGameboard.boardMatrix[1][4]).toBe(undefined)
+    expect(myGameboard.boardMatrix[1][3].hitTimes).toBe(0)
 })
 
 test("Ships receive attacks correctly and are sunk when received enough hits", () => {
-    myGameboard.receiveAttack(3, 1)
-    expect(myGameboard.boardMatrix[3][1].hitTimes).toBe(1)
-    expect(myGameboard.boardMatrix[3][1].isSunk()).toBe(false)
-    myGameboard.receiveAttack(2, 1)
-    expect(myGameboard.boardMatrix[3][1].isSunk()).toBe(true)
+    myGameboard.receiveAttack(1, 3)
+    expect(myGameboard.boardMatrix[1][3].hitTimes).toBe(1)
+    expect(myGameboard.boardMatrix[1][3].isSunk()).toBe(false)
+    myGameboard.receiveAttack(1, 2)
+    expect(myGameboard.boardMatrix[1][2].isSunk()).toBe(true)
 })
 
 test("Ship is added correctly on column", () => {
     myGameboard.addShip(5, 5, 3, "col");
     expect(typeof myGameboard.boardMatrix[5][5]).toBe('object')
     expect(myGameboard.boardMatrix[5][5].length).toBe(3)
-    expect(myGameboard.boardMatrix[5][6].length).toBe(3)
-    expect(myGameboard.boardMatrix[5][7].length).toBe(3)
-    expect(myGameboard.boardMatrix[5][8]).toBe(undefined)
-
+    expect(myGameboard.boardMatrix[6][5].length).toBe(3)
+    expect(myGameboard.boardMatrix[7][5].length).toBe(3)
+    expect(myGameboard.boardMatrix[8][5]).toBe(undefined)
 }) 
+
+function showGameboard() {
+    for( let i = 0; i<= 9; i++) {
+        let currString = '';
+        for( let j = 0; j<= 9; j++) {
+            if(myGameboard.boardMatrix[i][j] == undefined) {
+                currString += 'e '; 
+            } else if(typeof myGameboard.boardMatrix[i][j] == "string") {
+                currString += myGameboard.boardMatrix[i][j];
+            } else {
+                if(myGameboard.hitMatrix[i][j] == 'h') {
+                    currString += myGameboard.hitMatrix[i][j] + ' ';
+                } else {
+                    currString += myGameboard.boardMatrix[i][j].length + ' '; 
+                }
+            }
+        }
+        console.log(currString)
+    }
+}
+
+test("Length 4 ship is added correct on last column bottom side", () => {
+    myGameboard.addShip(6, 9, 4, "col")
+    expect(typeof myGameboard.boardMatrix[6][9]).toBe('object')
+    expect(myGameboard.boardMatrix[6][9].length).toBe(4)
+    expect(myGameboard.boardMatrix[7][9].length).toBe(4)
+    expect(myGameboard.boardMatrix[8][9].length).toBe(4)
+    expect(myGameboard.boardMatrix[9][9].length).toBe(4)
+    expect(myGameboard.boardMatrix[5][9]).toBe(undefined)
+    myGameboard.receiveAttack(9, 9)
+    myGameboard.receiveAttack(9, 8)
+    expect(myGameboard.boardMatrix[6][9].hitTimes).toBe(1)
+    expect(myGameboard.boardMatrix[6][9].isSunk()).toBe(false)
+})
+
+test("if all sunk function is working", () => {
+    expect(myGameboard.allSunk()).toBe(false);
+})
+
+test("if checking for sunk is working after attacking all ships", () => {
+    myGameboard.receiveAttack(0, 0)
+    myGameboard.receiveAttack(5, 5)
+    myGameboard.receiveAttack(6, 5)
+    myGameboard.receiveAttack(7, 5)
+    myGameboard.receiveAttack(7, 9)
+    myGameboard.receiveAttack(8, 9)
+    myGameboard.receiveAttack(9, 9)
+    showGameboard();
+    expect(myGameboard.allSunk()).toBe(true);
+})
+
+test("if all sunk is false after adding another ship", () => {
+    myGameboard.addShip(7, 7, 1, "row");
+    expect(myGameboard.allSunk()).toBe(false);
+})
