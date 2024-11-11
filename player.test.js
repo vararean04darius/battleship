48f75069b3342a343f87e566969974dc00d80859
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2024-11-11 15:22:09 +0200

    add computer player type to test

--- player.test.js
+++ player.test.js
@@ -1,16 +1,19 @@
 
 const Player = require('./player')
 let player1;
+let player2;
 
-function initializePlayer() {
+function initializePlayers() {
     player1 = new Player("Miguel", "real");
+    player2 = new Player("Computer", "computer")
 }
 
 beforeAll( () => {
-    initializePlayer();
+    initializePlayers();
 })
 
-test("Player is initialized correctly", () => {
+test("Players are initialized correctly", () => {
     expect(typeof player1).toBe('object')
-    expect(player1.name).toBe("Miguel")
+    expect(player1.type).toBe("real")
+    expect(player2.type).toBe("computer")
 })
