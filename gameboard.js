5b2881664e323488a0244ba807bbc8ec214ac8a0
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2024-11-11 12:57:49 +0200

    gameboard tests and class improved

--- gameboard.js
+++ gameboard.js
@@ -1,6 +1,47 @@
+const Ship = require('./ship');
+
 class Gameboard {
     constructor() {
+        this.ships = 0;
+        this.hits = 0;
+        this.missed = 0;
+        //10x10 board
+        this.boardMatrix = new Array(10);
+        for(let i = 0; i < this.boardMatrix.length; i++) {
+            this.boardMatrix[i] = new Array(10);
+        }
+    }
+
+    addShip(x, y, length, direction) {
+        // x, y >= 0 <=9
+        // length >= 1 <= 4
+        // direction 'row' || 'col'       
+        let newShip = new Ship(length);
         
+        if(direction == 'row') {
+            while(length >= 1) {
+                this.boardMatrix[x][y] = newShip;
+                x++;
+                length--;
+            }
+        } else {
+            while(length >= 1) {
+                this.boardMatrix[x][y] = newShip;
+                y++;
+                length--;
+            }
+        }
+        this.ships++
+    }
+
+    receiveAttack(x, y) {
+        if(this.boardMatrix[x][y] != undefined) {
+            this.boardMatrix[x][y].hit();
+            this.hits++;
+        } else {
+            this.boardMatrix[x][y] = 'x';
+            this.missed++;
+        }
     }
 }
 
--- gameboard.test.js
+++ gameboard.test.js
@@ -10,4 +10,29 @@ beforeAll( () => {
 
 test("Gameboard is created correctly", () => {
     expect(typeof myGameboard).toBe('object')
+    expect(Array.isArray(myGameboard.boardMatrix)).toBe(true)
+    expect(myGameboard.boardMatrix.length).toBe(10)
+    expect(myGameboard.boardMatrix[0].length).toBe(10)
+    expect(myGameboard.boardMatrix[0][0]).toBe(undefined)
+})
+
+test("Ship with length 1 is added correctly", () => {
+    myGameboard.addShip(0, 0, 1, "row")
+    expect(typeof myGameboard.boardMatrix[0][0]).toBe('object')
+    expect(myGameboard.boardMatrix[0][0].length).toBe(1)
+    expect(myGameboard.boardMatrix[0][1]).toBe(undefined)
+})
+
+test("Ship with length 2 is added correctly", () => {
+    myGameboard.addShip(2, 1, 2, "row")
+    expect(typeof myGameboard.boardMatrix[2][1]).toBe('object')
+    expect(typeof myGameboard.boardMatrix[3][1]).toBe('object')
+    expect(myGameboard.boardMatrix[1][1]).toBe(undefined)
+    expect(myGameboard.boardMatrix[4][1]).toBe(undefined)
+    expect(myGameboard.boardMatrix[3][1].hitTimes).toBe(0)
+    myGameboard.receiveAttack(3, 1)
+    expect(myGameboard.boardMatrix[3][1].hitTimes).toBe(1)
+    expect(myGameboard.boardMatrix[3][1].isSunk()).toBe(false)
+    myGameboard.receiveAttack(2, 1)
+    expect(myGameboard.boardMatrix[3][1].isSunk()).toBe(true)
 })
